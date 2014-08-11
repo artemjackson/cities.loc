31c475ea4b34243fce9b59046422d25d16cfba23
artemjackson <artemjackson95@gmail.com>
2014-08-11 11:40:06 +0300

    gMaps added

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
         $('#map_canvas').gmap().bind('init', function (ev, map) {
-            $('#map_canvas').gmap('addMarker', {'position': '57.7973333, 12.0502107', 'bounds': true}).click(function () {
+            $('#map_canvas').gmap('addMarker', {'position': '53.07, 25.59', 'bounds': true}).click(function () {
                 $('#map_canvas').gmap('openInfoWindow', {'content': 'Hello World!'}, this);
             });
         });
